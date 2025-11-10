c03979fb8cabf3e412daaddbc082b64ecd389cb7
Deepak Kasala <deepakkasala14@gmail.com>
2025-11-10 08:57:40 +0530

    fix: Correct import path for Last30DayExpenses component

--- client/src/pages/Dashboard/Home.jsx
+++ client/src/pages/Dashboard/Home.jsx
@@ -11,7 +11,7 @@ import { addThousandsSeparator } from "../../utils/helper";
 import RecentTransactions from "../../components/Dashboard/RecentTransactions";
 import FinanceOverview from "../../components/Dashboard/FinanceOverview";
 import ExpenseTransactions from "../../components/Dashboard/ExpenseTransactions";
-import Last30DayExpenses from "../../components/Dashboard/last30DayExpenses";
+import Last30DayExpenses from "../../components/Dashboard/Last30DayExpenses";
 import RecentIncomeWithChart from "../../components/Dashboard/RecentIncomeWithChart";
 import RecentIncome from "../../components/Dashboard/RecentIncome";
 
